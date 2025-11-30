6bea2d240684852a58ddcb9b4ceae8b50ebdd111
Noayo1 <nyo1254@gmail.com>
2025-11-30 18:37:52 +0200

    initial commit

---
+++ project/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Starting Lavagna..."
+
+java -Xms64m -Xmx128m \
+ -Ddatasource.dialect="${DB_DIALECT}" \
+ -Ddatasource.url="${DB_URL}" \
+ -Ddatasource.username="${DB_USER}" \
+ -Ddatasource.password="${DB_PASS}" \
+ -Dspring.profiles.active="${SPRING_PROFILE}" \
+ -jar /app/target/lavagna-jetty-console.war
