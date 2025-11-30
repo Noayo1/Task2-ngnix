11b1b8c9165ce052e21c51c1de1921848671d9f2
Noayo1 <nyo1254@gmail.com>
2025-11-30 20:57:48 +0200

    sql connection with docker compose

--- project/entrypoint.sh
+++ project/entrypoint.sh
@@ -1,11 +1,21 @@
-#!/bin/bash
+#!/bin/sh
+set -e
 
-echo "Starting Lavagna..."
+echo "Waiting for MySQL at ${DB_HOST}:3306..."
 
-java -Xms64m -Xmx128m \
- -Ddatasource.dialect="${DB_DIALECT}" \
- -Ddatasource.url="${DB_URL}" \
- -Ddatasource.username="${DB_USER}" \
- -Ddatasource.password="${DB_PASS}" \
- -Dspring.profiles.active="${SPRING_PROFILE}" \
- -jar /app/target/lavagna-jetty-console.war
+while ! nc -z ${DB_HOST} 3306; do
+  echo "MySQL not ready yet... retrying in 2 seconds"
+  sleep 2
+done
+
+echo "MySQL is ready. Starting Lavagna..."
+
+exec java \
+  -Xms64m \
+  -Xmx128m \
+  -Ddatasource.dialect="${DB_DIALECT}" \
+  -Ddatasource.url="${DB_URL}" \
+  -Ddatasource.username="${DB_USER}" \
+  -Ddatasource.password="${DB_PASS}" \
+  -Dspring.profiles.active="${SPRING_PROFILE}" \
+  -jar /app/target/lavagna-jetty-console.war --headless
